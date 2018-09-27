37106093a0596d7f80d60a7912c25ee5ab7c8803
yukminnie <dadiaomin@126.com>
2018-09-27 09:52:38 +0800

    初步实现

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$(p1).on('click', function() {
+    $(images).css({
+        transform: 'translateX(0)'
+    })
+})
+ $(p2).on('click', function() {
+    $(images).css({
+        transform: 'translateX(-500px)'
+    })
+})
+$(p3).on('click', function() {
+    $(images).css({
+        transform: 'translateX(-1000px)'
+    })
+})
