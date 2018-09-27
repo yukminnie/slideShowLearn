746b5f3b4f089d05682cae043b1029bdec9ba585
yukminnie <dadiaomin@126.com>
2018-09-27 09:58:33 +0800

    左边距实现(据说有bug,暂时未发现);注意和CSS的区别, 主要是变量引号和结尾是否有分号

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 $(p1).on('click', function() {
     $(images).css({
-        transform: 'translateX(0)'
+        'margin-left': 0
     })
 })
  $(p2).on('click', function() {
     $(images).css({
-        transform: 'translateX(-500px)'
+        'margin-left': '-500px'
     })
 })
 $(p3).on('click', function() {
     $(images).css({
-        transform: 'translateX(-1000px)'
+        'margin-left': '-1000px'
     })
 })
